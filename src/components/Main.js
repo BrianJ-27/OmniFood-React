55edcbe1b48f58f38dbc337318257334a559c5c2
Nestor Padilla <nrpadilla2@gmail.com>
2021-08-05 16:57:40 -0400

    Update SectionForm to use react functions and hooks instead of classes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,23 @@
-import SectionFeatures from './main-elements/SectionFeatures'
-import SectionMeals from './main-elements/SectionMeals'
-import SectionSteps from './main-elements/SectionSteps'
-import SectionCities from './main-elements/SectionCities'
-import SectionTestimonials from './main-elements/SectionTestimonials'
-import SectionPlans from './main-elements/SectionPlans'
-import SectionForm from './main-elements/SectionForm'
-
+import SectionFeatures from './main-elements/SectionFeatures';
+import SectionMeals from './main-elements/SectionMeals';
+import SectionSteps from './main-elements/SectionSteps';
+import SectionCities from './main-elements/SectionCities';
+import SectionTestimonials from './main-elements/SectionTestimonials';
+import SectionPlans from './main-elements/SectionPlans';
+import SectionFormV2 from './main-elements/SectionFormV2';
 
 const MainContent = () => {
-    return (
-       <main>
-        <SectionFeatures/>
-        <SectionMeals/>
-        <SectionSteps/>
-        <SectionCities/>
-        <SectionTestimonials/>
-        <SectionPlans/>
-        <SectionForm/>
-       </main> 
-    )
-}
+  return (
+    <main>
+      <SectionFeatures />
+      <SectionMeals />
+      <SectionSteps />
+      <SectionCities />
+      <SectionTestimonials />
+      <SectionPlans />
+      <SectionFormV2 />
+    </main>
+  );
+};
 
 export default MainContent;
---
+++ src/components/main-elements/SectionFormV2.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+
+const SectionFormV2 = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    useremail: '',
+    topic: 'Friends',
+    isChecked: true,
+    message: ''
+  });
+
+  const handleUpdate = (field, { target }) => {
+    setFormData({
+      ...formData,
+      [field]: target.value
+    });
+  };
+
+  const handleSubmitForm = event => {
+    event.preventDefault();
+    alert(`Form Submission Details:
+            ${formData.username}
+            ${formData.useremail}
+            ${formData.topic}
+            ${formData.isChecked}
+            ${formData.message}
+            `);
+  };
+
+  return (
+    <section className="section-form">
+      <div className="row">
+        <h2>We're happy to hear from you</h2>
+      </div>
+      <div className="row">
+        <form method="post" action="#" className="contact-form" onSubmit={handleSubmitForm}>
+          <fieldset className="row">
+            <ul>
+              <li className="inner-flex">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                  value={formData.username}
+                  onChange={e => handleUpdate('username', e)}
+                  required
+                />
+              </li>
+
+              <li className="inner-flex">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your email"
+                  value={formData.useremail}
+                  onChange={e => handleUpdate('useremail', e)}
+                  required
+                />
+              </li>
+              <li className="inner-flex">
+                <label htmlFor="find-us">How did you find us?</label>
+                <select
+                  name="find-us"
+                  id="find-us"
+                  value={formData.topic}
+                  onChange={e => handleUpdate('topic', e)}
+                >
+                  <option value="friends">Friends</option>
+                  <option value="search">Search Engine</option>
+                  <option value="ad">Advertisement</option>
+                  <option value="other">Other</option>
+                </select>
+              </li>
+              <li className="inner-flex">
+                <label htmlFor="news">Newsletter</label>
+                <div className="input-container">
+                  <input
+                    type="checkbox"
+                    name="news"
+                    id="news"
+                    checked={formData.newsLetter}
+                    onChange={e => handleUpdate('newsLetter', e)}
+                  />
+                  Yes Please!
+                </div>
+              </li>
+              <li className="inner-flex">
+                <label htmlFor="message">Drop us a line</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Your Message"
+                  value={formData.message}
+                  onChange={e => handleUpdate('message', e)}
+                ></textarea>
+              </li>
+              <li className="submit-btn-container">
+                <input type="submit" value="Send it!" />
+              </li>
+            </ul>
+          </fieldset>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default SectionFormV2;
