68451d4f21f21fc0db97b878c3da9e4af6c78b26
Brian Johnson <johnson8727@gmail.com>
2021-08-12 16:05:17 -0400

    finished styling the city section

--- src/components/main-elements/SectionCities.js
+++ src/components/main-elements/SectionCities.js
@@ -1,50 +1,37 @@
-import { citiData } from '../../data';
+import { citiData } from "../../data";
 import { IoPersonSharp, IoStarSharp, IoLogoTwitter } from "react-icons/io5";
-import { makeStyles } from '@material-ui/core/styles';
-
-const useCityStyles =makeStyles({
-  cityIcon: {
-    color: '#17cf45',
-    display: 'inline-block',
-    width: '30px',
-    fontSize: '120%',
-    marginRight: '-1px',
-    lineHeight: '120%',
-    verticalAlign: 'middle',
-    marginTop: '-5px'
-  },
-});
 
 const citiInfo = citiData;
 
 const SectionCities = () => {
-
-  const classes = useCityStyles();
-
   return (
     <section className="section-cities" id="cities">
-      <div className="row">
-        <h2>We're currently in these cities</h2>
-      </div>
-      <div className='flex__container--city'>
-        {citiInfo.map(({ id, img, city, review1, review2, link }) => (
-          <div key={id} className='col'>
-            <img src={img} alt="Lisbon" />
-            <h3>{city}</h3>
-            <div className="city-feature">
-              <IoPersonSharp className={classes.cityIcon} />
-              {review1}
+      <div className="wrapper">
+        <div className="row">
+          <h2 className="title__secondary">We're currently in these cities</h2>
+        </div>
+        <div className="flex__container">
+          {citiInfo.map(({ id, img, city, review1, review2, link }) => (
+            <div key={id} className="grid__col--3">
+              <img src={img} alt="Lisbon" />
+              <h3 className="title__tertiary">{city}</h3>
+              <div className="city-feature">
+                <IoPersonSharp className="icon__city" />
+                {review1}
+              </div>
+              <div className="city-feature">
+                <IoStarSharp className="icon__city" />
+                {review2}
+              </div>
+              <div className="city-feature">
+                <IoLogoTwitter className="icon__city" />
+                <a style={{ color: "#17cf45" }} href="#g">
+                  {link}
+                </a>
+              </div>
             </div>
-            <div className="city-feature">
-              <IoStarSharp className={classes.cityIcon} />
-              {review2}
-            </div>
-            <div className="city-feature">
-              <IoLogoTwitter className={classes.cityIcon} />
-              <a style={{color: '#17cf45'}} href="#g">{link}</a>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
