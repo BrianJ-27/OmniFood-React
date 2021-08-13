3974407c88b5b8e87572d8f28a1632b804f9b6f0
Brian Johnson <johnson8727@gmail.com>
2021-08-13 14:36:39 -0400

    finished styling footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,90 +1,62 @@
-import { makeStyles } from '@material-ui/core/styles';
-import { IoLogoFacebook, IoLogoTwitter, IoLogoGoogle, IoLogoInstagram} from "react-icons/io5";
-
-const useFooterStyles = makeStyles({
-  footerBg: {
-    background: '#333',
-    padding: '2em',
-    fontSize: '90%',
-    color: '#888'
-  },
-  flexRow: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    maxWidth: '1140px',
-    margin: '1em auto'
-  },
-  footerNav:{
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  firstLi: {
-    marginTop: '.5em',
-    marginBottom: '.5em',
-    marginRight: '1.2em'
-  },
-  footerNavLi: {
-    margin: '.5em 1.2em'
-  },
-  bottomFooter: {
-    textAlign: 'center'
-  }
-})
+import {
+  IoLogoFacebook,
+  IoLogoTwitter,
+  IoLogoGoogle,
+  IoLogoInstagram,
+} from "react-icons/io5";
 
 const Footer = () => {
-
-  const classes = useFooterStyles();
-
   return (
-    <footer className={classes.footerBg}>
-      <div className={classes.flexRow}>
-        <div className="col span-1-of-2">
-          <ul className={classes.footerNav}>
-            <li className={classes.firstLi}>
-              <a href="#1">About us</a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#2">Blog</a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#3">Press</a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#4">iOS App</a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#5">Android App</a>
-            </li>
-          </ul>
+    <footer>
+      <div className="wrapper">
+        <div className="flex__container">
+          <div className="footer__col">
+            <ul className="footer__nav">
+              <li className="footer__item">
+                <a className="footer__link" href="#1">About us</a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#2">Blog</a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#3">Press</a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#4">iOS App</a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#5">Android App</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__col">
+            <ul className="footer__nav">
+              <li className="footer__item">
+                <a className="footer__link" href="#6">
+                  <IoLogoFacebook />
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#7">
+                  <IoLogoTwitter />
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#8">
+                  <IoLogoGoogle />
+                </a>
+              </li>
+              <li className="footer__item">
+                <a className="footer__link" href="#9">
+                  <IoLogoInstagram />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="col span-1-of-2">
-          <ul className={classes.footerNav}>
-            <li className={classes.footerNavLi}>
-              <a href="#6">
-              <IoLogoFacebook/>
-              </a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#7">
-              <IoLogoTwitter/>
-              </a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#8">
-              <IoLogoGoogle/>
-              </a>
-            </li>
-            <li className={classes.footerNavLi}>
-              <a href="#9">
-              <IoLogoInstagram/>
-              </a>
-            </li>
-          </ul>
+        <div className="footer__bar">
+          <p>Copyright &copy; 2015 by Omnifood. All rights reserved</p>
         </div>
-      </div>
-      <div className={classes.bottomFooter}>
-        <p>Copyright &copy; 2015 by Omnifood. All rights reserved</p>
       </div>
     </footer>
   );
