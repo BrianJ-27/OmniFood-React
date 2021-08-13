a2321f535c68d6bbdca3bfff489ee9fcca574bf9
Brian Johnson <johnson8727@gmail.com>
2021-08-13 15:42:33 -0400

    added 4 pages to test React Router

---
+++ src/components/header-elements/OurCities.js
@@ -0,0 +1,14 @@
+import HeaderPage from '../Header'
+import FooterPage from '../Footer'
+
+const OurCitiesPage = () => {
+    return (
+        <>
+        <HeaderPage/>
+        <main>This is Our Cities Page</main>
+        <FooterPage/>
+        </>
+    );
+}
+
+export default OurCitiesPage;
